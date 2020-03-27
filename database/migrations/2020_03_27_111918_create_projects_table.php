a7d296e3583083808da8af2ca477eb283f0caad3
Chiyu Hsieh <ChiyuH1996@gmail.com>
2020-03-27 19:37:04 +0800

    made migration for Projects

---
+++ database/migrations/2020_03_27_111918_create_projects_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Projects', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title',30);
+            $table->string('content',255);
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Projects');
+    }
+}
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,21 +1,10 @@
 @extends('layout')
 @section('mainContent')
 	<h1>List Projects</h1>
-	<table class="table-striped">
-		<thead>
-			<tr>
-				<th>Title</th>
-			</tr>
-		</thead>
-		<tbody>
-			@foreach($projects as $project)
-			<tr>
-				<td><a href="projects/{{ $project->id }}">{{$project->title}}</a>
-				</td>
-			</tr>
-			@endforeach
-		</tbody>
-		<td><a href="{{ url('projects/create') }}">create</a>
-		</td>
-	</table>
+	@foreach($projects as $project)
+	<div>
+		<h3><a href="projects/{{ $project->id }}">{{$project->title}}</a></h3>
+		<a>{{$project->content}}</a>
+	</div>
+	@endforeach
 @endsection
