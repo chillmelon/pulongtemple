835686f3bcaf271f66c857beb066e0c0b1059e10
Chiyu Hsieh <ChiyuH1996@gmail.com>
2020-05-11 21:09:56 +0800

    fixing payment_type

--- resources/views/donates/ecpay.blade.php
+++ resources/views/donates/ecpay.blade.php
@@ -8,21 +8,21 @@ window.addEventListener('message', function (e) {
 <script src="https://payment-stage.ecpay.com.tw/Scripts/SP/ECPayPayment_1.0.0.js"
  data-MerchantID= "<?php echo$MerchantID?>"
  data-SPToken= "<?php echo$SPToken?>"
- data-PaymentType="CREDIT "
+ data-PaymentType="CREDIT"
  data-PaymentName="信用卡"
  data-CustomerBtn="0" >
 </script>
 <script src="https://payment-stage.ecpay.com.tw/Scripts/SP/ECPayPayment_1.0.0.js"
  data-MerchantID= "<?php echo$MerchantID?>"
  data-SPToken= "<?php echo$SPToken?>"
- data-PaymentType="ATM "
+ data-PaymentType="ATM"
  data-PaymentName="ATM"
  data-CustomerBtn="0" >
 </script>
 <script src="https://payment-stage.ecpay.com.tw/Scripts/SP/ECPayPayment_1.0.0.js"
  data-MerchantID= "<?php echo$MerchantID?>"
  data-SPToken= "<?php echo$SPToken?>"
- data-PaymentType="CVS "
+ data-PaymentType="CVS"
  data-PaymentName="超商代碼"
  data-CustomerBtn="0" >
 </script>
