13af1c46c29613ecc29db149120f8318e77fab6c
navibluer <navibluer@gmail.com>
2020-05-20 22:03:16 +0800

    content layout

--- resources/views/projects/project.blade.php
+++ resources/views/projects/project.blade.php
@@ -1,25 +1,27 @@
 @extends("layouts.outer")
+@yield("summary")
 @section("nav-mid")
 <!-- medium navbar -->
 <nav class="navbar navbar-expand navbar-dark sticky-top nav-mid">
   <div class="pages">
     <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link " href="#">專案內容<span class="sr-only">(current)</span></a>
+      <li class="nav-item @yield('content-active')">
+        <a class="nav-link " href="#">專案內容@yield("content-current")</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="update.html">更新</a>
+      <li class="nav-item @yield('update-active')">
+        <a class="nav-link" href="update.html">更新@yield("update-current")</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="comment.html">留言</a>
+      <li class="nav-item @yield('comment-active')">
+        <a class="nav-link" href="comment.html">留言@yield("comment-current")</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="QA.html">常見問答</a>
+      <li class="nav-item @yield('QA-active')">
+        <a class="nav-link @yield('QA-active')" href="QA.html">常見問答@yield("QA-current")</a>
       </li>
     </ul>
   </div>
   <div class="donate">
-    <a class="btn btn-3 wave-btn" href="/donate/{{ $id }}"><span>贊&emsp;助&emsp;$</span></a>
+    <a class="btn btn-3 wave-btn" href="#"><span>贊&emsp;助&emsp;$</span></a>
   </div>
 </nav>
 @endsection
+@yield("sub-content")
