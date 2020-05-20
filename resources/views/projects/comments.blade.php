2c14f19276bc4995bb3af0f7bf47250aacbe597f
navibluer <navibluer@gmail.com>
2020-05-21 00:00:01 +0800

    project layout

--- resources/views/projects/comments.blade.php
+++ resources/views/projects/comments.blade.php
@@ -2,5 +2,137 @@
 
 @section("sub-content")
 
+<div class="container-fluid bottom-content">
+  <div class='row'>
+    <div class='col-12 col-lg-8'>
+      <!-- rank table -->
+      <div class="rank">
+        <table>
+          <tr>
+            <th>Rank</th>
+            <th>贊助人</th>
+            <th>金額</th>
+          </tr>
+          <tr>
+            <td>#1</td>
+            <td class="usr"><img class="usr-img" src="../image/pulongTemple.png">Pulong</td>
+            <td>1000</td>
+          </tr>
+          <tr>
+            <td>#2</td>
+            <td class="usr"><img class="usr-img" src="../image/user/nux.jpg">Nux</td>
+            <td>623</td>
+          </tr>
+          <tr>
+            <td>#3</td>
+            <td class="usr"><img class="usr-img" src="../image/user/a.png">Johnson</td>
+            <td>66</td>
+          </tr>
+          <tr>
+            <td>#4</td>
+            <td class="usr"><img class="usr-img" src="../image/user/b.png">Johnson</td>
+            <td>66</td>
+          </tr>
+          <tr>
+            <td>#5</td>
+            <td class="usr"><img class="usr-img" src="../image/user/c.png">Johnson</td>
+            <td>66</td>
+          </tr>
+          <tr>
+            <td>#6</td>
+            <td class="usr"><img class="usr-img" src="../image/user/d.png">客家人</td>
+            <td>5</td>
+          </tr>
+        </table>
+      </div>
+      <!-- comment -->
+      <div class="cmt-all">
+        <div class="cmt">
+          <img class="cmt-img" src="../image/user/d.png">
+          <div class="cmt-content">
+            <a class="usr-name">Hater</a><br>爛透了爛透了爛透了爛透了爛透了爛透了爛透了爛透了爛透了爛透了爛透了爛透了爛透了爛透了爛透了爛透了爛透了爛透了爛透了爛透了爛透了爛透了爛透了爛透了爛透了爛透了爛透了爛透了爛透了爛透了
+          </div>
+        </div>
+        <div class="cmt">
+          <img class="cmt-img" src="../image/user/d.png">
+          <div class="cmt-content">
+            <a class="usr-name">Hater</a><br>爛透了爛透了爛透了爛透了爛透了爛透了爛透了爛透了爛透了
+          </div>
+        </div>
+        <div class="cmt">
+          <img class="cmt-img" src="../image/user/d.png">
+          <div class="cmt-content">
+            <a class="usr-name">Hater</a><br> Strange transport. Listening to ~~~ is somehow like eavesdropping on another person's memories. Favorite track: It's a Rainy Day On The Cosmic Shore.
+          </div>
+        </div>
+        <div class="cmt">
+          <img class="cmt-img" src="../image/user/d.png">
+          <div class="cmt-content">
+            <a class="usr-name">Hater</a><br>爛透了
+          </div>
+        </div>
+      </div>
+      <!-- user gallery -->
+      <div class="gallery">
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+        <div class="usr-imgbox"><img class="usr-img" src="../image/user/a.png"></div>
+      </div>
+    </div>
+  </div>
+</div>
 
 @endsection
