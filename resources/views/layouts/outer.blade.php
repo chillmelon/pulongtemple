0ab2a0072762877ea15856782840b345b1f90dca
navibluer <navibluer@gmail.com>
2020-05-22 00:03:47 +0800

    login-or-not

--- resources/views/layouts/outer.blade.php
+++ resources/views/layouts/outer.blade.php
@@ -31,7 +31,30 @@
     </div>
     <ul class="login">
 
-      @yield("login&out")
+      {{-- has login --}}
+      @auth
+        <li class="">
+          <a class="btn" href="{{ url('/dashboard') }}">{{ auth()->user()->name }}</a>
+        </li>
+        <li class="">
+          <a class="btn" href="{{ route('logout') }}"
+             onclick="event.preventDefault();
+            document.getElementById('logout-form').submit();">
+            登出
+          </a>
+          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+              @csrf
+          </form>
+        </li>
+      {{-- has log out --}}
+      @else
+        <li class="">
+          <a class="btn" href="{{ route('login') }}">登入</a>
+        </li>
+        <li class="">
+          <a class="btn" href="{{ route('register') }}">註冊</a>
+        </li>
+      @endauth
 
     </ul>
     </div>
