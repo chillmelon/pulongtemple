13fa5fa8c47bdf8f6ca13dfa8c5439df990ba4b1
navibluer <navibluer@gmail.com>
2020-05-23 03:32:15 +0800

    login ascii

--- resources/views/thankyou.blade.php
+++ resources/views/thankyou.blade.php
@@ -1,5 +1,29 @@
-@extends('layout')
-@section('mainContent')
-<h1>Thank you.<h1>
-<a href="/">see other projects</a>
+@extends("auth.login-button")
+@section('body')
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="thx-box col-12">
+      <pre class="thx">
+ ___________ __    __      __     _____  ___  __   ___
+("     _   ")" |  | "\    /""\   (\"   \|"  \|/"| /  ")
+ )__/  \\__(:  (__)  :)  /    \  |.\\   \    (: |/   /
+    \\_ /   \/      \/  /' /\  \ |: \.   \\  |    __/
+    |.  |   //  __  \\ //  __'  \|.  \    \. (// _  \
+    \:  |  (:  (  )  :)   /  \\  \    \    \ |: | \  \
+     \__|   \__|  |__(___/    \___)___|\____\|__|  \__)
+               ___  ___ ______   ____  ____
+              |"  \/"  /    " \ ("  _||_ " |
+               \   \  // ____  \|   (  ) : |
+                \\  \/  /    ) :|:  |  | . )
+                /   (: (____/ // \\ \__/ //
+               /   / \        /  /\\ __ //\
+              |___/   \"_____/  (__________)
+
+      </pre>
+    </div>
+    <div class="col-8 col-md-6 col-xl-4">
+      <a class="btn btn-5" href="/">看看其他專案</a>
+    </div>
+  </div>
+</div>
 @endsection
