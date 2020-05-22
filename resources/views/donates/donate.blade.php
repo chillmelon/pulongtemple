cffc0db502a008d3631ea358f249369562ae642c
navibluer <navibluer@gmail.com>
2020-05-23 00:22:32 +0800

    donate page modify

--- resources/views/donates/donate.blade.php
+++ resources/views/donates/donate.blade.php
@@ -1,54 +1,61 @@
 @extends("layouts.outer")
-
 @section("login-button")
-  <a class="btn" style="visibility: hidden;" href="#"></a>
+<a class="btn" style="visibility: hidden;" href="#"></a>
 @endsection
 @section("body")
-<div class="row justify-content-center">
-  <div class="donate-form col-12 col-md-10 col-lg-8 col-xl-6">
-    <div class="inform">
-      <h4>說明一下吼</h4>
-    </div>
-    {{-- login --}}
-    <div class="col-12 no-login">
-      <a class="btn btn-5" href="{{ route('login') }}"><span>以會員身分贊助</span></a>
-    </div>
-    <div class="c-box">
-      <div class="card custom-card">
-        <div class="card-header">贊助專案：{{ $project_info['title'] }}</div>
-        <div class="card-body">
-          <form method="POST" action="{{ route('donates.new', $project_info['id']) }}" name="donation">
-            @csrf
-            贊助人
-            <input type="hidden" name="project_id" value = "{{ $project_info['id'] }}">
-            <input type="hidden" name="user_id" value = "{{ auth()->user()['id'] }}">
-            <input class="form-control @error('name') is-invalid @enderror" type="text" name="name" value = "{{ auth()->user()['name'] }}" >
-            @error('name')
-            <span class="invalid-feedback" role="alert">
-              <strong>{{ $message }}</strong>
-            </span>
-            @enderror
-            金額 $
-            <input class="form-control @error('amount') is-invalid @enderror" type="integer" name="amount">
-            @error('amount')
-            <span class="invalid-feedback" role="alert">
-              <strong>{{ $message }}</strong>
-            </span>
-            @enderror
-            E-mail
-            <input class="form-control @error('email') is-invalid @enderror" type="text" name="email" value="{{ auth()->user()['email'] }}">
-            @error('email')
-            <span class="invalid-feedback" role="alert">
-              <strong>{{ $message }}</strong>
-            </span>
-            @enderror
-            留言(選填)
-            <textarea class="form-control comment" type="text" name="comment"></textarea>
-            <div class="send">
-              <button class="btn btn-5">確定</button>
-            </div>
-
-          </form>
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-12 col-md-10 col-lg-8 col-xl-6">
+      <div class="inform">
+        <h4>登入會員身分？</h4>
+        <br>
+        <h5>
+          &emsp;登入會員可以記錄每一筆的贊助紀錄。<br>
+          &emsp;也可以發起屬於自己的募資專案。
+        </h5>
+      </div>
+      <div class="to-login">
+        <a class="btn btn-5" href="{{ route('login') }}"><span>註冊或登入</span></a>
+      </div>
+      <div class="continue">
+        <h4>太麻煩了，直接贊助 👇</h4>
+      </div>
+      <div class="c-box donate-form ">
+        <div class="card custom-card">
+          <div class="card-header">贊助專案：{{ $project_info['title'] }}</div>
+          <div class="card-body">
+            <form method="POST" action="{{ route('donates.new', $project_info['id']) }}" name="donation">
+              @csrf
+              贊助人
+              <input type="hidden" name="project_id" value = "{{ $project_info['id'] }}">
+              <input type="hidden" name="user_id" value = "{{ auth()->user()['id'] }}">
+              <input class="form-control @error('name') is-invalid @enderror" type="text" name="name" value = "{{ auth()->user()['name'] }}" >
+              @error('name')
+              <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+              </span>
+              @enderror
+              金額$
+              <input class="form-control @error('amount') is-invalid @enderror" type="integer" name="amount">
+              @error('amount')
+              <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+              </span>
+              @enderror
+              E-mail(將會收到專案更新資訊)
+              <input class="form-control @error('email') is-invalid @enderror" type="text" name="email" value="{{ auth()->user()['email'] }}">
+              @error('email')
+              <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+              </span>
+              @enderror
+              留言(選填)
+              <textarea class="form-control comment" type="text" name="comment"></textarea>
+              <div class="send">
+                <button class="btn btn-5">確定</button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
