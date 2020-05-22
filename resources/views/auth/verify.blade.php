4ddadd8849d344a3aebb452c1248e4275bac025c
navibluer <navibluer@gmail.com>
2020-05-23 01:06:18 +0800

    verify page modify

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,28 +1,27 @@
 @extends("auth.no-login-button")
-
 @section("body")
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8 c-box">
-            <div class="card custom-card">
-                <div class="card-header">認證您的E-mail</div>
-
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            新的認證信已寄到您的信箱。
-                        </div>
-                    @endif
-
-                    認證信已經寄到您的信箱。<br>
-                    如果沒收到的話 👇
-                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
-                        @csrf
-                        <button type="submit" class="btn btn-5">要求新的一封認證信</button>
-                    </form>
-                </div>
-            </div>
+  <div class="row justify-content-center">
+    <div class="col-md-8 c-box">
+      <div class="card custom-card">
+        <div class="card-header">認證您的E-mail</div>
+        <div class="card-body">
+          @if (session('resent'))
+          <div class="alert alert-success" role="alert">
+            新的一封認證信已寄出。
+          </div>
+          @endif
+          <h4>
+          認證信已經寄到您的信箱。
+          </h4>
+          如果沒收到的話 👇
+          <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+            @csrf
+            <button type="submit" class="btn btn-5">要求新的一封認證信</button>
+          </form>
         </div>
+      </div>
     </div>
+  </div>
 </div>
 @endsection
