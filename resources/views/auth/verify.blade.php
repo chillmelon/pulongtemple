e6459ae51a86901e471b822db93dbc459e87cf21
navibluer <navibluer@gmail.com>
2020-05-23 01:18:53 +0800

    verify page modify

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -7,11 +7,11 @@
         <div class="card-header">E-mail 認證中...</div>
         <div class="card-body">
           @if (session('resent'))
-          <div class="answer">
-            新的一封認證信已寄出。
+          <div class="new-email">
+            <h4>新的認證信已寄出。</h4>
           </div>
           @endif
-          <h4>認證信已經寄到您的信箱。</h4>
+          <h4>認證信已寄出，請去信箱中點擊認證連結。</h4>
           <br>
           如果沒收到的話 👇
           <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
