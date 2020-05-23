d0e6cb25bf26b4af41be7094852bf3553fbd7aed
navibluer <navibluer@gmail.com>
2020-05-23 18:50:31 +0800

    index page modify

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,17 +1,38 @@
-@extends('layout')
-@section('content')
-<div class="title m-b-md">
-	埔隆宮
-</div>
-<div class="flex-center position-ref">
-	<table style="border: 1px solid black;">
-	@foreach($projects as $project)
-		<tr>
-			<td><a href="projects/{{$project['id']}}">{{$project['title']}}</a></td>
-			<td><img src="/storage/{{ $project['cover'] }}"></td>
-			<td><a>{{ $project['progress'] }}</a></td>
-		</tr>
-	@endforeach
-</table>
+@extends('auth.login-button')
+@section('body')
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-12 col-md-10 col-lg-8 col-xl-6">
+      <div class="pltp-box">
+        <pre class="pltp">
+██████╗ ██╗   ██╗██╗      ██████╗ ███╗   ██╗ ██████╗
+██╔══██╗██║   ██║██║     ██╔═══██╗████╗  ██║██╔════╝
+██████╔╝██║   ██║██║     ██║   ██║██╔██╗ ██║██║  ███╗
+██╔═══╝ ██║   ██║██║     ██║   ██║██║╚██╗██║██║   ██║
+██║     ╚██████╔╝███████╗╚██████╔╝██║ ╚████║╚██████╔╝
+╚═╝      ╚═════╝ ╚══════╝ ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝
+
+████████╗███████╗███╗   ███╗██████╗ ██╗     ███████╗
+╚══██╔══╝██╔════╝████╗ ████║██╔══██╗██║     ██╔════╝
+   ██║   █████╗  ██╔████╔██║██████╔╝██║     █████╗
+   ██║   ██╔══╝  ██║╚██╔╝██║██╔═══╝ ██║     ██╔══╝
+   ██║   ███████╗██║ ╚═╝ ██║██║     ███████╗███████╗
+   ╚═╝   ╚══════╝╚═╝     ╚═╝╚═╝     ╚══════╝╚══════╝
+
+        </pre>
+      </div>
+      <div class="rank">
+        <table>
+          @foreach($projects as $project)
+          <tr>
+            <td><a class="btn btn-5" href="projects/{{$project['id']}}">{{$project['title']}}</a></td>
+            <td><img src="/storage/{{ $project['cover'] }}"></td>
+            <td><a class="btn btn-5">{{ $project['progress'] }}</a></td>
+          </tr>
+          @endforeach
+        </table>
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
