03fb8a20cf45de4fbd82ce9b74a7297a9dcd8255
navibluer <navibluer@gmail.com>
2020-05-24 02:22:00 +0800

    personal page layout

--- resources/views/member/Donations.blade.php
+++ resources/views/member/Donations.blade.php
@@ -1,22 +1,28 @@
 @extends("auth.login-button")
 @section("body")
-<h1>我的捐款</h1>
-<table>
-<tr>
-	<td>交易單號</td>
-	<td>贊助專案</td>
-	<td>金額</td>
-	<td>留言</td>
-	<td>時間</td>
-</tr>
-@foreach($donates as $donation)
-<tr>
-	<td>{{ $donation->uuid }}</td>
-	<td><a href="/projects/{{ $donation->project->id }}">{{ $donation->project->title }}</a></td>
-	<td>{{ $donation->amount }}</td>
-	<td>{{ $donation->comment }}</td>
-	<td>{{ $donation->created_at }}</td>
-</tr>
-@endforeach
-</table>
+<div class="container-fluid">
+  <div class="row justify-content-center">
+    <div class="col-md-10 col-xl-6" style="padding-top: 32px;">
+      <h1>我的贊助</h1>
+      <table>
+      <tr>
+      	<td>交易單號</td>
+      	<td>贊助專案</td>
+      	<td>金額</td>
+      	<td>留言</td>
+      	<td>時間</td>
+      </tr>
+      @foreach($donates as $donation)
+      <tr>
+      	<td>{{ $donation->uuid }}</td>
+      	<td><a href="/projects/{{ $donation->project->id }}">{{ $donation->project->title }}</a></td>
+      	<td>{{ $donation->amount }}</td>
+      	<td>{{ $donation->comment }}</td>
+      	<td>{{ $donation->created_at }}</td>
+      </tr>
+      @endforeach
+      </table>
+    </div>
+  </div>
+</div>
 @endsection
--- resources/views/member/Projects.blade.php
+++ resources/views/member/Projects.blade.php
@@ -1,21 +1,27 @@
 @extends("auth.login-button")
 @section("body")
-<table>
-<tr>
-	<td>專案名稱</td>
-	<td>現在金額</td>
-	<td>目標金額</td>
-	<td>開始時間</td>
-	<td>結束時間</td>
-</tr>
-@foreach($projects as $project)
-<tr>
-	<td><a class="btn btn-5" href="/projects/{{ $project->id }}">{{ $project->title }}</a></td>
-	<td>{{ $project->total_amount }}</td>
-	<td>{{ $project->goal }}</td>
-	<td>{{ $project->created_at }}</td>
-	<td>{{ $project->deadline }}</td>
-</tr>
-@endforeach
-</table>
+<div class="container-fluid">
+  <div class="row justify-content-center">
+    <div class="col-md-10 col-xl-6" style="padding-top: 32px;">
+      <table>
+        <tr>
+          <td>專案名稱</td>
+          <td>現在金額</td>
+          <td>目標金額</td>
+          <td>開始時間</td>
+          <td>結束時間</td>
+        </tr>
+        @foreach($projects as $project)
+        <tr>
+          <td><a class="btn btn-5" href="/projects/{{ $project->id }}">{{ $project->title }}</a></td>
+          <td>{{ $project->total_amount }}</td>
+          <td>{{ $project->goal }}</td>
+          <td>{{ $project->created_at }}</td>
+          <td>{{ $project->deadline }}</td>
+        </tr>
+        @endforeach
+      </table>
+    </div>
+  </div>
+</div>
 @endsection
