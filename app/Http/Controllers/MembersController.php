e548ccbc6fc4f437470bb15de56006ec8f1a7028
chillmelon <chiyuh1996@gmail.com>
2020-05-24 16:50:34 +0800

    route dashboard

--- app/Http/Controllers/MembersController.php
+++ app/Http/Controllers/MembersController.php
@@ -11,23 +11,19 @@ class MembersController extends Controller
     	$this->middleware('auth');
         $this->memberService = $memberService;
     }
+    public function index(){
+        $profile = $this->memberService->profile();
+        return view('member.dashboard',['profile'=>$profile]);
+    }
     public function donates(){
     	$user_id = auth()->user()['id'];
     	$donates = $this->memberService->donates($user_id);
     	return view('member.Donations',['donates'=>$donates]);
     }
-    public function index()
-    {
-        return view('member.dashboard');
-    }
     public function projects(){
     	$user_id = auth()->user()['id'];
     	$projects = $this->memberService->projects($user_id);
     	return view('member.Projects',['projects'=>$projects]);
-    }
-    public function profile(){
-        $profile = $this->memberService->profile();
-        return view('member.Profile',['profile'=>$profile]);
     }
     public function update(Request $request){
         $profile = $this->memberService->update($request);
