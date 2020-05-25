33158d762272eabf144c3e46b9a6bba6345850da
navibluer <navibluer@gmail.com>
2020-05-25 20:47:56 +0800

    script moved to layout

--- resources/views/layouts/outer.blade.php
+++ resources/views/layouts/outer.blade.php
@@ -9,6 +9,10 @@
   @yield("meta")
 
   <title>PulongTemple 專案內容</title>
+  {{-- cropper --}}
+  <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+  <script src="{{ asset('/js/cropper.js') }}"></script>
   <!-- bootstrap -->
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js" type="text/javascript"></script>
