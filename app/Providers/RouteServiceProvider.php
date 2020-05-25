8539d3272b2da2f578836b118fe5cac9a8acbe5d
chillmelon <chiyuh1996@gmail.com>
2020-05-25 21:26:34 +0800

    RouteServiceProvider added DASHBOARD

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -23,6 +23,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     public const HOME = '/home';
 
+	public const DASHBOARD = '/dashboard';
     /**
      * Define your route model bindings, pattern filters, etc.
      *
