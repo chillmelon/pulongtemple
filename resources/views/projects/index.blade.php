6e3cd70d3e996e318adc319197614ddf9fb8260a
navibluer <navibluer@gmail.com>
2020-05-30 18:27:20 +0800

    index card

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -20,13 +20,34 @@
    ╚═╝    ╚══════╝ ╚═╝     ╚═╝ ╚═╝      ╚══════╝ ╚══════╝
         </pre>
       </div>
+      {{-- product --}}
+      <div class="row justify-content-center">
+        @foreach($projects as $project)
+        <div class="product-box col-12 col-md-6">
+          <div class="product custom-bdr" onclick="location.href='projects/{{ $project->id }}';">
+            <div class="product-head">
+              {{ $project->title }}
+            </div>
+            <div class="product-body">
+              <p>{{ $project->content }}</p>
+              <div class="product-progress"></div>
+            </div>
+          </div>
+        </div>
+        @endforeach
+      </div>
+
+      {{--  --}}
+      <br>
+      <br>
+      <br>
       <div class="rank">
         <table>
           @foreach($projects as $project)
           <tr>
-			  <td><a class="btn btn-5" href="projects/{{ $project->id }}">{{ $project->title }}</a></td>
-			  <td><img src="/storage/{{ $project->cover }}"></td>
-			  <td><a>{{ $project->progress }}</a></td>
+        <td><a class="btn btn-5" href="projects/{{ $project->id }}">{{ $project->title }}</a></td>
+        <td><img src="/storage/{{ $project->cover }}"></td>
+        <td><a>{{ $project->progress }}</a></td>
           </tr>
           @endforeach
         </table>
