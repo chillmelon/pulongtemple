5a1b6d45df151f3d43aba49f22e01f81ce44349f
navibluer <navibluer@gmail.com>
2020-06-04 00:12:52 +0800

    update page layout

--- resources/views/projects/updates.blade.php
+++ resources/views/projects/updates.blade.php
@@ -16,7 +16,7 @@ active
         <img class="width-img" src="https://s3-ap-northeast-1.amazonaws.com/zeczec-prod/asset_179720_image_original.jpg?1589528089">
         <span>S6 Pure 掃地機器人【✨提・前・出・貨・公告✨】原訂於 6 月開始出貨，在我們積極和工廠聯繫後，目前爭取到「前三波預購訂單」主機提前出貨！📍提前出貨方案請見以下——【5/19 開始依序出貨】► 限時 24 hr，加贈可水洗＋一次...</span>
         <div class="more-btn">
-          <a class="btn btn-4" href="/updates/1">更多內容...</a>
+          <a class="btn" href="/updates/1">更多內容...</a>
         </div>
       </div>
       {{-- update-2 --}}
@@ -26,7 +26,7 @@ active
         <img class="width-img" src="https://s3-ap-northeast-1.amazonaws.com/zeczec-prod/asset_178563_image_original.png?1589269176">
         <span>🎉賀！金額金額突破 1,000 萬 🔥⚡️預購人數突破 1,000 人 ⚡️1,000 多人的支持，代表著石頭科技帶給這麼多家庭更美好的生活，往後也繼續讓我們用科技，成為你的生活中的質感和幸福感吧。【預購限定】抽獎活動熱烈進行中！下單...</span>
         <div class="more-btn">
-          <a class="btn btn-4" href="#">更多內容...</a>
+          <a class="btn" href="#">更多內容...</a>
         </div>
       </div>
       {{-- update-3 --}}
@@ -36,7 +36,7 @@ active
         <img class="width-img" src="https://s3-ap-northeast-1.amazonaws.com/zeczec-prod/asset_177316_image_original.jpg?1588852266">
         <span>💬「小孩放電完，身為媽媽的我也沒電打掃了...」💬「剛和老公買房，很希望透過掃地機器人提升生活品質」💬「生完小孩飯都沒辦法好好吃，更別說打掃」💬「打掃就放過自己，我想要留下更多時間陪家人」———來自大家的吶喊我們都聽到了，讓 S6 P...</span>
         <div class="more-btn">
-          <a class="btn btn-4" href="#">更多內容...</a>
+          <a class="btn" href="#">更多內容...</a>
         </div>
       </div>
 
