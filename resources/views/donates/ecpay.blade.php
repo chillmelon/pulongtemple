2e6651622ed97564d46ad1008c46cc0fb582b32d
navibluer <navibluer@gmail.com>
2020-06-08 22:05:15 +0800

    ecpay btn

--- resources/views/donates/ecpay.blade.php
+++ resources/views/donates/ecpay.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.outer')
 @section('body')
 <div class="row justify-content-center" style="padding-top: 32px">
+  <button class="btn ec-btn" onclick="checkOut('CREDIT')">信用卡付款</button>
+  <button class="btn ec-btn" onclick="checkOut('ATM')">ATM付款</button>
+  <button class="btn ec-btn" onclick="checkOut('CVS')">超商代碼付款</button>
+  {{-- script --}}
   <script type="text/javascript" src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js" > $(function () {
   window.addEventListener('message', function (e) {
   alert("訂單結果資訊:" + e.data);
@@ -13,7 +17,6 @@
   data-PaymentName="信用卡"
   data-CustomerBtn="1" >
   </script>
-  <button class="btn" onclick="checkOut('CREDIT')">信用卡</button>
   <script src="https://payment.ecpay.com.tw/Scripts/SP/ECPayPayment_1.0.0.js"
   data-MerchantID= "<?php echo$MerchantID?>"
   data-SPToken= "<?php echo$SPToken?>"
@@ -21,7 +24,6 @@
   data-PaymentName="ATM"
   data-CustomerBtn="1" >
   </script>
-  <button class="btn" onclick="checkOut('ATM')">信用卡</button>
   <script src="https://payment.ecpay.com.tw/Scripts/SP/ECPayPayment_1.0.0.js"
   data-MerchantID= "<?php echo$MerchantID?>"
   data-SPToken= "<?php echo$SPToken?>"
@@ -29,6 +31,5 @@
   data-PaymentName="超商代碼"
   data-CustomerBtn="1" >
   </script>
-  <button class="btn" onclick="checkOut('CVS')">信用卡</button>
 </div>
 @endsection
