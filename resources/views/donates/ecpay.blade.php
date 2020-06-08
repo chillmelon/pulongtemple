e422932906e243b8f79577138242874962ea0ea1
navibluer <navibluer@gmail.com>
2020-06-08 22:00:12 +0800

    ecpay btn

--- resources/views/donates/ecpay.blade.php
+++ resources/views/donates/ecpay.blade.php
@@ -5,27 +5,30 @@
   window.addEventListener('message', function (e) {
   alert("訂單結果資訊:" + e.data);
   <?php echo "thank you";?>
-  }); </script>
+  });</script>
   <script src="https://payment.ecpay.com.tw/Scripts/SP/ECPayPayment_1.0.0.js"
   data-MerchantID= "<?php echo$MerchantID?>"
   data-SPToken= "<?php echo$SPToken?>"
   data-PaymentType="CREDIT"
   data-PaymentName="信用卡"
-  data-CustomerBtn="0" >
+  data-CustomerBtn="1" >
   </script>
+  <button class="btn" onclick="checkOut('CREDIT')">信用卡</button>
   <script src="https://payment.ecpay.com.tw/Scripts/SP/ECPayPayment_1.0.0.js"
   data-MerchantID= "<?php echo$MerchantID?>"
   data-SPToken= "<?php echo$SPToken?>"
   data-PaymentType="ATM"
   data-PaymentName="ATM"
-  data-CustomerBtn="0" >
+  data-CustomerBtn="1" >
   </script>
+  <button class="btn" onclick="checkOut('ATM')">信用卡</button>
   <script src="https://payment.ecpay.com.tw/Scripts/SP/ECPayPayment_1.0.0.js"
   data-MerchantID= "<?php echo$MerchantID?>"
   data-SPToken= "<?php echo$SPToken?>"
   data-PaymentType="CVS"
   data-PaymentName="超商代碼"
-  data-CustomerBtn="0" >
+  data-CustomerBtn="1" >
   </script>
+  <button class="btn" onclick="checkOut('CVS')">信用卡</button>
 </div>
 @endsection
