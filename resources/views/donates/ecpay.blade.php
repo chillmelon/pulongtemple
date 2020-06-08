97f39a15b4f194be945421017ba3cc9ce0e37920
navibluer <navibluer@gmail.com>
2020-06-08 20:51:25 +0800

    ecpay update

--- resources/views/donates/ecpay.blade.php
+++ resources/views/donates/ecpay.blade.php
@@ -2,31 +2,32 @@
 @section('body')
 {{-- @extends('layout') --}}
 {{-- @section('content') --}}
-<script type="text/javascript" src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js" > $(function () {
-window.addEventListener('message', function (e) {
-	alert("訂單結果資訊:" + e.data);
-	<?php echo "thank you";?>
-}); </script>
-<script src="https://payment.ecpay.com.tw/Scripts/SP/ECPayPayment_1.0.0.js"
- data-MerchantID= "<?php echo$MerchantID?>"
- data-SPToken= "<?php echo$SPToken?>"
- data-PaymentType="CREDIT"
- data-PaymentName="信用卡"
- data-CustomerBtn="0" >
-</script>
-<script src="https://payment.ecpay.com.tw/Scripts/SP/ECPayPayment_1.0.0.js"
- data-MerchantID= "<?php echo$MerchantID?>"
- data-SPToken= "<?php echo$SPToken?>"
- data-PaymentType="ATM"
- data-PaymentName="ATM"
- data-CustomerBtn="0" >
-</script>
-<script src="https://payment.ecpay.com.tw/Scripts/SP/ECPayPayment_1.0.0.js"
- data-MerchantID= "<?php echo$MerchantID?>"
- data-SPToken= "<?php echo$SPToken?>"
- data-PaymentType="CVS"
- data-PaymentName="超商代碼"
- data-CustomerBtn="0" >
-</script>
-
+<div class="row justify-content-center">
+    <script type="text/javascript" src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js" > $(function () {
+    window.addEventListener('message', function (e) {
+        alert("訂單結果資訊:" + e.data);
+    <?php echo "thank you";?>
+    }); </script>
+    <script src="https://payment.ecpay.com.tw/Scripts/SP/ECPayPayment_1.0.0.js"
+    data-MerchantID= "<?php echo$MerchantID?>"
+    data-SPToken= "<?php echo$SPToken?>"
+    data-PaymentType="CREDIT"
+    data-PaymentName="信用卡"
+    data-CustomerBtn="0" >
+    </script>
+    <script src="https://payment.ecpay.com.tw/Scripts/SP/ECPayPayment_1.0.0.js"
+    data-MerchantID= "<?php echo$MerchantID?>"
+    data-SPToken= "<?php echo$SPToken?>"
+    data-PaymentType="ATM"
+    data-PaymentName="ATM"
+    data-CustomerBtn="0" >
+    </script>
+    <script src="https://payment.ecpay.com.tw/Scripts/SP/ECPayPayment_1.0.0.js"
+    data-MerchantID= "<?php echo$MerchantID?>"
+    data-SPToken= "<?php echo$SPToken?>"
+    data-PaymentType="CVS"
+    data-PaymentName="超商代碼"
+    data-CustomerBtn="0" >
+    </script>
+</div>
 @endsection
