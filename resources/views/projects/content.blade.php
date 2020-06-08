5584bdc036176216afe5a111ac272eb9e3c81a2a
navibluer <navibluer@gmail.com>
2020-06-09 00:17:58 +0800

    content summary layout update

--- resources/views/projects/content.blade.php
+++ resources/views/projects/content.blade.php
@@ -16,19 +16,33 @@
         埔隆宮<span style="font-family: serif;"> - </span>炭烤土司大王
         <br>
         </h3>
-        <h6 style="font-size: 12px;">by Pulong Temple</h6>
+        <h6 style="font-size: 12px;"> by Pulong Temple</h6>
         <hr class="hr-prime">
       </div>
       <!-- status -->
       <div class="row status">
         <!-- goal -->
         <div class="goal col-6 col-sm-8 col-lg-6">
-          <h4>NT${{ $project->amount }}</h4>
-          <p style="font-size: 12px;">目標 NT${{ $project->goal }}</p>
+          <div class="max-w-260">
+            <h4 class="inline-b">NT$</h4>
+            <h4 class="inline-b ab-rb">{{ $project->amount }}</h4>
+          </div>
+          <div class="max-w-260">
+            <span class="inline-b fs-12">目標 NT$</span>
+            <span class="inline-b ab-rb fs-12">{{ $project->goal }}</span>
+          </div>
+
           <hr class="hr-prime">
-          <h4>{{ $project->supporters }}<small> 人贊助</small></h4>
-          <hr class="hr-prime">
-          <h4>{{ $project->days_left }}<small> 天剩餘</small></h4>
+
+          <div class="max-w-260">
+            <h4 class="inlin-b">{{ $project->supporters }}</h4>
+            <span class="inlin-b ab-rb">人贊助</span>
+          </div>
+          {{-- <hr class="hr-prime"> --}}
+          <div class="max-w-260">
+            <h4 class="inlin-b">{{ $project->days_left }}</h4>
+            <span class="inlin-b ab-rb">天剩餘</span>
+          </div>
         </div>
         <!-- Progress bar -->
         <div class="col-6 col-sm-4 col-lg-6">
