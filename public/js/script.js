c13583acabfc96410b37ce395ec29377d28f3cf7
navibluer <navibluer@gmail.com>
2020-06-14 18:00:51 +0800

    script modified

--- public/js/script.js
+++ public/js/script.js
@@ -6,7 +6,5 @@ function moveProgressBar(pid) {
     var animationLength = 2500;
     // on page load, animate percentage bar to data percentage length
     // .stop() used to prevent animation queueing
-    console.log(progressTotal);
-    console.log('#'+pid+' .progress-bar');
     $('#'+pid+' .progress-bar').stop().animate({left: progressTotal}, animationLength);
 }
