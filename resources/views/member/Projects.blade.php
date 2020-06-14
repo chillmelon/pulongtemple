5589b95dba586804b17085a04d5aa55de665c6fb
navibluer <navibluer@gmail.com>
2020-06-14 22:18:36 +0800

    my donation & porject page modified

--- resources/views/member/Projects.blade.php
+++ resources/views/member/Projects.blade.php
@@ -3,7 +3,7 @@
 <div class="container-fluid">
   <div class="row justify-content-center">
     <div class="my_p col-md-10 col-xl-6">
-      <h3>My Project</h3>
+      <h3 class="ff-2P">My Project</h3>
       @foreach($projects as $project)
       <div class="p-card-box">
         <div class="p-card custom-bdr">
@@ -13,9 +13,8 @@
           </div>
           <div class="p-card-body">
             <a class="btn my-project" href="/projects/{{ $project->id }}">{{ $project->title }}</a>
-            <div class="p-card-nt my-project-nt">NT${{ $project->amount }}<br>/{{ $project->goal }}</div>
+            <div class="p-card-nt ff-2P fs-12">NT$ {{ $project->amount }}<br>/ {{ $project->goal }}</div>
           </div>
-          {{-- <div class="p-card-footer"></div> --}}
         </div>
       </div>
       @endforeach
