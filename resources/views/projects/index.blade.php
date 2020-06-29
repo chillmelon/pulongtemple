c9db6b8221f79bcfce24eff6d0edc131bd6c708f
navibluer <navibluer@gmail.com>
2020-06-29 15:38:47 +0800

    meta image

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,6 +1,6 @@
 @section("meta")
 <meta itemprop="name" content="埔隆宮募資平台">
-<meta itemprop="image" content="https://photos.google.com/u/6/photo/AF1QipMnD_QEJNypNR7oQ9Da-qBXH1yyc-K18a87szIL">
+<meta itemprop="image" content="https://lh3.googleusercontent.com/pw/ACtC-3cMImHYZqnVJbP3GFWKriM1b4p997cX-nHvuBNIcxeUFn5PRHDXhOB_kePcQktI2pZ6vzxw9e_Fvcs37OqtkhRI4pinQ1gP0YEbo413ZvSqnfNvpEOVw5ChP6JNh5JZndfNYiFJfjYrOGQzEErIAJE=w1250-h774-no?authuser=6">
 <meta itemprop="description" content="埔里鎮的每個人給我10塊錢，我就有80萬了。
 對，做事情要花錢。公共性的事情不是政府做就是政府發錢給民間做，所以還是政府在做。政府做事就是慢，程序很多。所以我們募資，直接把錢給要做事的人。
 
@@ -12,7 +12,7 @@
 {{-- facebook --}}
 <meta property="og:title" content="埔隆宮募資平台" >
 <meta property="og:url" content="https://pulongtemple.com/">
-<meta property="og:image" content="https://photos.google.com/u/6/photo/AF1QipMnD_QEJNypNR7oQ9Da-qBXH1yyc-K18a87szIL">
+<meta property="og:image" content="https://lh3.googleusercontent.com/pw/ACtC-3cMImHYZqnVJbP3GFWKriM1b4p997cX-nHvuBNIcxeUFn5PRHDXhOB_kePcQktI2pZ6vzxw9e_Fvcs37OqtkhRI4pinQ1gP0YEbo413ZvSqnfNvpEOVw5ChP6JNh5JZndfNYiFJfjYrOGQzEErIAJE=w1250-h774-no?authuser=6">
 <meta property="og:description" content="埔里鎮的每個人給我10塊錢，我就有80萬了。
 對，做事情要花錢。公共性的事情不是政府做就是政府發錢給民間做，所以還是政府在做。政府做事就是慢，程序很多。所以我們募資，直接把錢給要做事的人。
 
