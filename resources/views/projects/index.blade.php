4d0b5d557ee6f7e789e10d7f6138a0d37ba1b7e1
navibluer <navibluer@gmail.com>
2020-06-29 15:16:29 +0800

    meta description modify

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,12 +1,26 @@
 @section("meta")
 <meta itemprop="name" content="埔隆宮募資平台">
 <meta itemprop="image" content="https://photos.google.com/u/6/photo/AF1QipMnD_QEJNypNR7oQ9Da-qBXH1yyc-K18a87szIL">
-<meta itemprop="description" content="埔隆宮募資平台">
+<meta itemprop="description" content="埔里鎮的每個人給我10塊錢，我就有80萬了。
+對，做事情要花錢。公共性的事情不是政府做就是政府發錢給民間做，所以還是政府在做。政府做事就是慢，程序很多。所以我們募資，直接把錢給要做事的人。
+
+有人在燒草很臭？大家募資買個樹枝粉碎機。
+
+想要一個滑板場？大家募資租一個倉庫來玩。
+
+為埔里好的事我們都要支持！">
 {{-- facebook --}}
 <meta property="og:title" content="埔隆宮募資平台" >
 <meta property="og:url" content="https://pulongtemple.com/">
 <meta property="og:image" content="https://photos.google.com/u/6/photo/AF1QipMnD_QEJNypNR7oQ9Da-qBXH1yyc-K18a87szIL">
-<meta property="og:description" content="埔隆宮募資平台" >
+<meta property="og:description" content="埔里鎮的每個人給我10塊錢，我就有80萬了。
+對，做事情要花錢。公共性的事情不是政府做就是政府發錢給民間做，所以還是政府在做。政府做事就是慢，程序很多。所以我們募資，直接把錢給要做事的人。
+
+有人在燒草很臭？大家募資買個樹枝粉碎機。
+
+想要一個滑板場？大家募資租一個倉庫來玩。
+
+為埔里好的事我們都要支持！" >
 @endsection
 
 @extends('auth.login-button')
