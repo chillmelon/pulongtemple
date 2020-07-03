b94cec60e9217be8b6e03a6dac652c8bb73c1cfa
navibluer <navibluer@gmail.com>
2020-07-03 17:14:25 +0800

    register > login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,62 +5,6 @@
   <div class="row justify-content-center">
     <div class="col-12">
       <div class="row">
-        {{-- login --}}
-        <div class="col-lg-6 c-box">
-          <h5 style="padding: 16px;">已經是會員</h5>
-          <div class="card custom-card login-box custom-bdr">
-            <div class="card-header">登入</div>
-            <div class="card-body">
-              <form method="POST" action="{{ route('login') }}">
-                @csrf
-                <div class="form-group row">
-                  <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail</label>
-                  <div class="col-md-6">
-                    <input id="email" type="email" class="form-control custom-bdr @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-                    @error('email')
-                    <span class="invalid-feedback" role="alert">
-                      <strong>{{ $message }}</strong>
-                    </span>
-                    @enderror
-                  </div>
-                </div>
-                <div class="form-group row">
-                  <label for="password" class="col-md-4 col-form-label text-md-right">密碼</label>
-                  <div class="col-md-6">
-                    <input id="password" type="password" class="form-control custom-bdr @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-                    @error('password')
-                    <span class="invalid-feedback" role="alert">
-                      <strong>{{ $message }}</strong>
-                    </span>
-                    @enderror
-                  </div>
-                </div>
-                <div class="form-group row">
-                  <div class="col-md-6 offset-md-4">
-                    <div class="form-check">
-                      <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                      <label class="form-check-label" for="remember">
-                        記住我
-                      </label>
-                    </div>
-                  </div>
-                </div>
-                <div class="form-group row mb-0 btn-box">
-                  <div class="col-md-6 offset-md-4 mg-bottom-16">
-                    <button type="submit" class="btn">
-                    登入
-                    </button>
-                  </div>
-                  <div class="col-md-6 offset-md-4">
-                    <a class="btn" href="{{ route('password.request') }}">
-                      忘記密碼?
-                    </a>
-                  </div>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
         {{-- register --}}
         <div class="col-lg-6 c-box">
           <h5 style="padding: 16px;">現在加入</h5>
@@ -113,6 +57,62 @@
                     <button type="submit" class="btn">
                     註冊
                     </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+        {{-- login --}}
+        <div class="col-lg-6 c-box">
+          <h5 style="padding: 16px;">已經是會員</h5>
+          <div class="card custom-card login-box custom-bdr">
+            <div class="card-header">登入</div>
+            <div class="card-body">
+              <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <div class="form-group row">
+                  <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail</label>
+                  <div class="col-md-6">
+                    <input id="email" type="email" class="form-control custom-bdr @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                    @error('email')
+                    <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label for="password" class="col-md-4 col-form-label text-md-right">密碼</label>
+                  <div class="col-md-6">
+                    <input id="password" type="password" class="form-control custom-bdr @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                    @error('password')
+                    <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <div class="col-md-6 offset-md-4">
+                    <div class="form-check">
+                      <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                      <label class="form-check-label" for="remember">
+                        記住我
+                      </label>
+                    </div>
+                  </div>
+                </div>
+                <div class="form-group row mb-0 btn-box">
+                  <div class="col-md-6 offset-md-4 mg-bottom-16">
+                    <button type="submit" class="btn">
+                    登入
+                    </button>
+                  </div>
+                  <div class="col-md-6 offset-md-4">
+                    <a class="btn" href="{{ route('password.request') }}">
+                      忘記密碼?
+                    </a>
                   </div>
                 </div>
               </form>
