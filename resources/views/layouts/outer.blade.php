7ac24fc6c71c762b7bce559c2425e7facff54d24
navibluer <navibluer@gmail.com>
2020-10-17 16:37:55 +0800

    bs4 rebuild

--- resources/views/layouts/outer.blade.php
+++ resources/views/layouts/outer.blade.php
@@ -14,7 +14,7 @@
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
     <script src="{{ asset('/js/cropper.js') }}"></script>
     <!-- bootstrap -->
-    <link href="{{asset('css/bootstrap-grid.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset('css/bootstrap.css')}}" rel="stylesheet" type="text/css" />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
     {{-- js --}}
     <script src="{{ asset('/js/script.js') }}"></script>
