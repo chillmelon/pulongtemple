67d03336fcac702226563cd6342a46f20c5c85cc
navibluer <navibluer@gmail.com>
2020-10-22 21:46:32 +0800

    rank table update

--- resources/views/projects/comments.blade.php
+++ resources/views/projects/comments.blade.php
@@ -38,7 +38,7 @@ active
       <!-- user gallery -->
       <div class="gallery">
         @foreach($gallary as $icon)
-        <div class="usr-imgbox"><img class="usr-img" src="{{asset('storage/'.$icon[ 'avatar' ])}}"></div>
+        <div class="p-2"><img class="usr-img" src="{{asset('storage/'.$icon[ 'avatar' ])}}"></div>
         @endforeach
       </div>
     </div>
