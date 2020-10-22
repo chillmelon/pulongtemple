ada913dbea3bf033fc58cbd5321e90643851c8ef
navibluer <navibluer@gmail.com>
2020-10-23 03:35:16 +0800

    rank table update

--- resources/views/projects/comments.blade.php
+++ resources/views/projects/comments.blade.php
@@ -9,7 +9,7 @@ active
       <!-- comment -->
       <div class="cmt-all">
         @foreach($randFive as $comment)
-        <div class="cmt">
+        <div class="cmt pb-4">
           <img class="cmt-img" src="{{asset('storage/'.$comment[ 'avatar' ])}}">
           <div class="cmt-content">
             <a class="usr-name">{{ $comment[ 'name' ] }}</a><br>{{ $comment[ 'comment' ] }}
@@ -19,7 +19,7 @@ active
         @endforeach
       </div>
       <!-- user gallery -->
-      <div class="gallery">
+      <div class="gallery pb-4">
         @foreach($gallary as $icon)
         <div class="p-2" style="display: inline-block"><img class="usr-img" src="{{asset('storage/'.$icon[ 'avatar' ])}}"></div>
         @endforeach
@@ -30,7 +30,7 @@ active
       @foreach($topFive as $donater)
       <div class="rank custom-bdr-3d p-3 mt-2">
         <h4 class="select-title pl-2"><I>{{$loop->index+1}}</I></h4>
-        {{-- <div><img class="usr-img" src="{{asset('storage/'.$donater[ 'avatar' ])}}"><span class="p-2">{{$donater[ 'name' ]}}</span class="p-2"></div> --}}
+        <div><img class="usr-img" src="{{asset('storage/'.$donater[ 'avatar' ])}}"><span class="p-2">{{$donater[ 'name' ]}}</span class="p-2"></div>
         <h6 class="pr-3">NT$ {{$donater[ 'amount' ]}}</h6>
       </div>
       @endforeach
