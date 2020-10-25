d00d1191c03b538fa2b81dbfa459a90fa0911544
navibluer <navibluer@gmail.com>
2020-10-25 18:30:38 +0800

    amount-btn update

--- resources/views/donates/select.blade.php
+++ resources/views/donates/select.blade.php
@@ -2,61 +2,57 @@
 @section("body")
 <div class="container">
   <div class="row justify-content-center">
-    <div class="col-md-10 col-lg-12">
+    <div class="col-md-10 col-lg-12 py-5">
       @auth
-      <br>
-      <br>
-      <div class="donate-box">
-        <div class="row">
-          {{-- Progress bar --}}
-          <div class="col-lg-4">
-            <div class="progress-box">
-              <div style="text-align: center;">
-                <h4>{{$project_info->title}}</h4>
-                {{-- circle --}}
-                <div style="height: 200px">
-                  <div class="circle-pg-box ab-center">
-                    <div class="circle-pg">
-                      <div class="circle-pg-inner">
-                        <div class="percent">{{$project_info->progress}}%</div>
-                        <div class="water" data-percent="{{$project_info->progress}}"></div>
-                        <div class="glare"></div>
-                      </div>
+      <div class="row">
+        <div class="col-lg-4 mt-lg-4">
+          {{-- Project status --}}
+          <div class="progress-box">
+            <div style="text-align: center;">
+              <h4>{{$project_info->title}}</h4>
+              {{--Progress circle --}}
+              <div style="height: 200px">
+                <div class="circle-pg-box ab-center">
+                  <div class="circle-pg">
+                    <div class="circle-pg-inner">
+                      <div class="percent">{{$project_info->progress}}%</div>
+                      <div class="water" data-percent="{{$project_info->progress}}"></div>
+                      <div class="glare"></div>
                     </div>
                   </div>
                 </div>
-                <div class="ff-2P">
-                  <h6>NT$ {{$project_info->amount}} <small><br>/&ensp;{{$project_info->goal}}</small></h5>
-                </div>
+              </div>
+              <div class="ff-2P">
+                <h6>NT$ {{$project_info->amount}} <small><br>/&ensp;{{$project_info->goal}}</small></h5>
               </div>
             </div>
           </div>
-          {{-- plan --}}
-          <div class="col-lg-8">
-            <div class="row">
-							<div class="col-lg-6">
-								<div class="custom-bdr-3d custom-bdr-3d-hover" onclick="location.href='/donate/{{$project_info->id}}'">
-                  <div class="serif-tc p-4">
-                    <h3 class="select-title"><I> 純贊助 </I></h3>
-                    <div class="ff-2P pt-2">NT$ ???</div>
-                    <div class="pt-2">已被贊助 {{$project_info->donors}} 次</div>
-                    <div class="select-content"><span>隨喜樂捐，不求回報。</span></div>
-                  </div>
-                </div>
-							</div>
-							@foreach($project_info->options->sortByDesc('order') as  $option)
-              <div class="col-lg-6">
-								<div class="custom-bdr-3d custom-bdr-3d-hover" onclick="location.href='/donate/option/{{$option->id}}'">
-                  <div class="serif-tc p-4">
-                    <h3 class="select-title"><I> {{$option->title}} </I></h3>
-                    <div class="ff-2P pt-2">NT$ {{$option->price}}</div>
-                    <div class="pt-2">已被贊助 {{$option->sold}} 次</div>
-                    <div class="select-content"><span>{{$option->content}}</span></div>
-                  </div>
+        </div>
+        {{-- plan --}}
+        <div class="col-lg-8">
+          <div class="row">
+            <div class="col-lg-6">
+              <div class="custom-bdr-3d custom-bdr-3d-hover" onclick="location.href='/donate/{{$project_info->id}}'">
+                <div class="serif-tc p-4">
+                  <h3 class="select-title"><I> 純贊助 </I></h3>
+                  <div class="ff-2P pt-2">NT$ ???</div>
+                  <div class="pt-2">已被贊助 {{$project_info->donors}} 次</div>
+                  <div class="select-content"><span>隨喜樂捐，不求回報。</span></div>
                 </div>
               </div>
-							@endforeach
             </div>
+            @foreach($project_info->options->sortByDesc('order') as  $option)
+            <div class="col-lg-6">
+              <div class="custom-bdr-3d custom-bdr-3d-hover" onclick="location.href='/donate/option/{{$option->id}}'">
+                <div class="serif-tc p-4">
+                  <h3 class="select-title"><I> {{$option->title}} </I></h3>
+                  <div class="ff-2P pt-2">NT$ {{$option->price}}</div>
+                  <div class="pt-2">已被贊助 {{$option->sold}} 次</div>
+                  <div class="select-content"><span>{{$option->content}}</span></div>
+                </div>
+              </div>
+            </div>
+            @endforeach
           </div>
         </div>
       </div>
